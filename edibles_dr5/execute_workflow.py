04401494c4ff230f95de0d8a4c06165ee970a943
Alexander Ebenbichler <alexander@ebenbichler.net>
2025-01-09 09:33:30 +0100

    s

--- edibles_dr5/execute_workflow.py
+++ edibles_dr5/execute_workflow.py
@@ -2,7 +2,7 @@ import nbformat
 from nbconvert.preprocessors import ExecutePreprocessor
 from importlib.resources import files
 
-notebook_filename = files('edibles_dr5')
+notebook_filename = files('edibles_dr5') / 'workflow.ipynb'
 
 with open(notebook_filename) as f:
     nb = nbformat.read(f, as_version=4)
