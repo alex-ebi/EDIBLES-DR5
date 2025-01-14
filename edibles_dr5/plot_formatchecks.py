0b087366655e4f5fb07df215da76af911276e07d
Alexander Ebenbichler <alexander@ebenbichler.net>
2025-01-14 16:54:08 +0100

    mkdir

--- edibles_dr5/plot_formatchecks.py
+++ edibles_dr5/plot_formatchecks.py
@@ -6,6 +6,7 @@ from astropy.io import fits
 from pprint import pprint
 import matplotlib.pyplot as plt
 from pathlib import Path
+from edibles_dr5.edr5_functions import get_wave_path
 
 calib_dir = paths.edr5_dir / 'calib_raw'
 plot_dir_flat = Path('/home/alex/diss_dibs/edibles_reduction/calib_check/flat')
@@ -18,6 +19,10 @@ for file in calib_dir.glob('*.fits'):
             data = [f[0].data]
         else:
             data = [f[1].data, f[2].data]
+    
+    wave, setting = get_wave_path(hdr)
+
+    print(f'{wave}_{setting}')
 
     for i, img in enumerate(data):
         im = plt.imshow(img.T)
@@ -28,7 +33,12 @@ for file in calib_dir.glob('*.fits'):
             plot_dir = plot_dir_flat
         else:
             continue
-        plt.savefig(plot_dir / f'{hdr['DATE'][:10]}_{i}.png')
+        if i == 0:
+            lu = 'l'
+        elif i == 1:
+            lu='u'
+        (plot_dir / f'{wave:.0f}_{setting}{lu}').mkdir(exist_ok=True)
+        plt.savefig(plot_dir / f'{wave:.0f}_{setting}{lu}' / f'{hdr['DATE'][:10]}.png')
         # plt.show()
         plt.close()
 
