79b2749300e5c5c489ae3d8a9ad00ea7cdad76e0
Alexander Ebenbichler <alexander@ebenbichler.net>
2025-04-09 10:34:22 +0200

    breakpoints flat histogram

--- edibles_dr5/flat_histogram.py
+++ edibles_dr5/flat_histogram.py
@@ -8,6 +8,9 @@ from importlib.resources import files
 
 file_dir = files('edibles_dr5') / 'supporting_data'
 
+breakpoint_file = files('edibles_dr5') / 'supporting_data/breakpoints_4.csv'
+breakpoints = pd.read_csv(breakpoint_file, index_col=0).loc[:,'MJD'].values
+
 # Files listing the complete flats over the time frame from 2014.01.01 to 2019.12.31.
 # The second part of the tuple is the lower exposure time limit of the flats.
 files = [
@@ -22,7 +25,8 @@ for file, min_exp in files:
     sub_df = df.loc[df.Exptime > min_exp]
     print(len(sub_df)/5)
 
-    plt.hist(df['TPL START'], bins=6, label='Full sample')
-    plt.hist(sub_df['TPL START'], bins=6, label='Sample with time limit')
+    plt.title(file)
+    plt.hist(df['MJD-OBS'], bins=breakpoints, label='Full sample')
+    plt.hist(sub_df['MJD-OBS'], bins=breakpoints, label='Sample with time limit')
     plt.legend()
     plt.show()
