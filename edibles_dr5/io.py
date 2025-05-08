ed21869c61a49463338c319e96791beec8cc2a6f
Alexander Ebenbichler <alexander@ebenbichler.net>
2025-05-08 15:46:54 +0200

    io functions and transformations

---
+++ edibles_dr5/io.py
@@ -0,0 +1,33 @@
+from astropy.io import fits
+from pathlib import Path
+import numpy as np
+from edibles_dr5 import transformations
+
+def read_spec(spec_path:Path):
+    if spec_path.parent.name == 'tell_corr':
+        with fits.open(spec_path) as f:
+            data = f[1].data
+
+        wave = transformations.angstrom_vac_to_air(data['lambda']*10000)
+        spec = np.array([wave, data['cflux']])
+
+    else:
+        with fits.open(spec_path) as f:
+            data = f[1].data
+
+        spec = np.array([data['WAVE'], data['FLUX']])
+    
+    return spec
+
+
+def read_spec_wn(spec_path:Path):
+    spec = read_spec(spec_path)
+    spec[0] = transformations.angstrom_to_wavenumber(spec[0])
+    
+    return spec
+
+def read_star_name_obs_time(spec_path:Path):
+    with fits.open(spec_path) as f:
+        hdr = f[0].header
+
+    return hdr['OBJECT'], hdr['DATE-OBS']
